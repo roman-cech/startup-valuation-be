6266db2c40c52bf6fd25f013958f7af557954a13
RomanCech <81cechroman@gmail.com>
2024-04-23 01:27:50 +0200

    Drools rule engine update.

--- src/main/kotlin/cz/utb/fai/drools/DroolsRuleEngine.kt
+++ src/main/kotlin/cz/utb/fai/drools/DroolsRuleEngine.kt
@@ -2,30 +2,38 @@ package cz.utb.fai.drools
 
 import cz.utb.fai.dto.StartupValuationReq
 import cz.utb.fai.dto.StartupValuationRes
-import org.kie.api.runtime.KieContainer
+import org.kie.api.KieServices
+import org.kie.api.runtime.KieSession
 import org.springframework.stereotype.Component
 
 private const val DROOLS_RES = "response"
-private const val DROOLS_LISTENER = "listener"
-
+private const val KIE_BASE = "startup-valuation"
 
 @Component
-class DroolsRuleEngine(
-    private val kieContainer: KieContainer
-) {
-    fun computeValuation(request: StartupValuationReq): StartupValuationRes =
-        kieContainer.newKieSession().let { kieSession ->
-            try {
-                StartupValuationRes().run {
-                    kieSession.addEventListener(TrackingAgendaEventListener())
-                    kieSession.setGlobal(DROOLS_RES, this)
-                    kieSession.setGlobal(DROOLS_LISTENER, TrackingAgendaEventListener())
-                    request.evidences.forEach { evidence -> kieSession.insert(evidence) }
-                    kieSession.fireAllRules()
-                    this
-                }
-            } finally {
-                kieSession.dispose()
+class DroolsRuleEngine {
+    private var kieSession: KieSession
+
+    init {
+        val kContainer = KieServices.Factory.get().kieClasspathContainer
+        kieSession = kContainer.getKieBase(KIE_BASE).newKieSession()
+        kieSession.addEventListener(AgendaListener())
+
+        kieSession.setGlobal(DROOLS_RES, StartupValuationRes())
+    }
+
+    fun computeValuation(request: StartupValuationReq): StartupValuationRes {
+        val result = StartupValuationRes()
+
+        try {
+            request.evidences.forEach { evidence ->
+                kieSession.insert(evidence)
             }
+
+            kieSession.fireAllRules()
+        } finally {
+            kieSession.dispose()
         }
+
+        return result
+    }
 }
